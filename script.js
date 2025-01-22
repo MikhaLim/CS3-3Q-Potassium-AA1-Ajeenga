f216bc1920ec0af8276fdc378f4c22938f6fdb90
MikhaLim <110205662+MikhaLim@users.noreply.github.com>
2025-01-22 20:33:58 +0800

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const I = 3.14; 
-document.write("Value of PI: " + I);
-document.write('<br><br>');
-
+document.write("Value of π: " + I);
+document.write('<br><br>')
 let radius = 6.378e6; 
 document.write("Radius: " + radius);
 document.write('<br><br>');
-
-
 let C = 2 * I * radius;
 document.write("The circumference is: " + C);
 document.write('<br><br>');
-
 let A = 4 * I * (radius ** 2);
 document.write("The surface area is: " + A);
 document.write('<br><br>');
-
-
 let V = (4 / 3) * I * (radius ** 3);
 document.write("The Volume is: " + V);
 document.write('<br><br>');
