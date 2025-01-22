771b91bc958fc7e81afd29f4dd8622c1c2f690c5
MikhaLim <110205662+MikhaLim@users.noreply.github.com>
2025-01-22 20:21:41 +0800

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const I = 3.14; 
-document.write("Value of π: " + I);
+document.write("Value of PI: " + I);
 document.write('<br><br>');
 
 let radius = 6.378e6; 
-document.write("Radius of the sphere: " + radius);
+document.write("Radius: " + radius);
 document.write('<br><br>');
 
 
 let C = 2 * I * radius;
-document.write("Circumference: " + C);
+document.write("The circumference is: " + C);
 document.write('<br><br>');
 
 let A = 4 * I * (radius ** 2);
-document.write("Surface Area: " + A);
+document.write("The surface area is: " + A);
 document.write('<br><br>');
 
 
 let V = (4 / 3) * I * (radius ** 3);
-document.write("Volume: " + V);
+document.write("The Volume is: " + V);
 document.write('<br><br>');
